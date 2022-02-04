5e14cb36f197e70c6492622618c2508621dfc374
shk3029 <shk30290@naver.com>
2022-02-04 23:18:58 +0900

    feat: 변경 감지와 병합(merge) - ** 변경 감지를 사용하자

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +16,24 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
 
+    /**
+        실무에서 엔티티를 변경할 때 항상 변경감지를 사용하자
+        병합을 사용하면 모든 필드가 다 바뀐다 (값이 없는건 null로 바꿈)
+     */
+
+    // 병합(merge)
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    // 변경감지
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
